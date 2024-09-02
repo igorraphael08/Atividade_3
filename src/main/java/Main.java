78e4f90b27c914d579aa9537c84c57cb5d146878
igor raphael <169068315+igorraphael08@users.noreply.github.com>
2024-09-02 12:18:57 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("insira sua senha");
+
+
+int senha = sc.nextInt();
+
+while() {
+
+ System.out.println("senha incorreta");
+
+
+
+
+  
+}
+
+
+
+
+
+    
+
+  sc.close();  
+  }
+
+
+  
+  
+}
